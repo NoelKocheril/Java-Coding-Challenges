6a9d2c547ddb18dac36e7dfc5391e77cc6e93bc3
Noel Kocheril <NoelKocheril@GMail.com>
2019-01-21 15:55:58 -0500

    Added Comments to Challenge 300

--- Practice/src/c_300/Generator.java
+++ Practice/src/c_300/Generator.java
@@ -5,47 +5,61 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Random;
 
 public class Generator {
 
+	// Number of Entries Created
 	private int numVoters;
+	
+	// Highest Possible Voter Number
+	// Pigeon Hole Principle will determine if there are duplicates
 	private int maxVoterNum;
+	
+	// Highest Candidate ID
 	private int maxCandidate_ID;
 	
-	
+	//Default Generator Settings	
 	public Generator() {
 		this.numVoters = 10000;
 		this.maxVoterNum = 2000000;
 		this.maxCandidate_ID = 10;
 	}
 	
+	// Controllable Values
 	public Generator(int numVoters, int maxVoterNum, int maxCandidate_ID) {
 		this.numVoters = numVoters;
 		this.maxVoterNum = maxVoterNum;
 		this.maxCandidate_ID = maxCandidate_ID;
 	}
 	
+	/**
+	 * 
+	 * @param fname - Path to the File
+	 * @throws IOException If an I/O Error Occurs
+	 */
 	public void generate(String fname) throws IOException {
+		// Checks if Path exists, if not creates one
 		File file = new File(fname);
 		if(!file.exists()) {
 	    	file.createNewFile();
 	    } 
 		
-		BufferedWriter out = new BufferedWriter(new FileWriter(file.getAbsolutePath()));
-	    Random rdm = new Random();
-	    
+		Random rdm = new Random();
 	    int voter_id, cand_id;
 	    String output = "";
 	    
+	    // Create numVoter Entries Format: VOTER_NUM, CAND_ID
 	    for(int i = 0; i < numVoters; i++) {
 	    	voter_id = rdm.nextInt(maxVoterNum)+1;
 	    	cand_id = rdm.nextInt(maxCandidate_ID);
 	    	//System.out.printf("Voter ID: %07d, Candidate ID: %02d \n", voter_id, cand_id);
+	    	// Reduce Number of Writes to File
 	    	output += String.format("%07d, %02d \n", voter_id, cand_id);
 	    }
-    	out.write(output);	
+	    // Create Writer and Writes to File
+	    BufferedWriter out = new BufferedWriter(new FileWriter(file.getAbsolutePath()));
+	    out.write(output);	
 	    out.close();
 	}
 
--- Practice/src/c_300/Solver.java
+++ Practice/src/c_300/Solver.java
@@ -1,40 +1,65 @@
 package c_300;
 
+/**
+ * This problem was asked by Uber.
+   
+   On election day, a voting machine writes data in the form (voter_id, candidate_id)
+   to a text file. Write a program that reads this file as a stream and returns the 
+   top 3 candidates at any given time. If you find a voter voting more than once, 
+   report this as fraud.
+ */
+
 import java.util.*;
 import java.io.File;
 import java.io.IOException;
 
 public class Solver {
 	public static void main(String[] args) throws IOException {
+
 		HashSet<Integer> voters = new HashSet<Integer>();
 		HashSet<Integer> fraud_voters = new HashSet<Integer>();
 		HashMap<Integer, Integer> cand_votes = new HashMap<Integer, Integer>();
+
 		String FName = "C:/temp/voter_info.txt";
 		File file = new File(FName);
+		// Generate File Information 
 		Generator gen = new Generator(30000, 3000000, 20);
 		gen.generate(FName);
 		Scanner in = new Scanner(file);
+		
+		// Go Through File
 		while(in.hasNextLine()) {
+			// Separate Each Line by , into 2 values (Voter_ID and Cand_ID)
 			String input[] = in.nextLine().split(",");
 			int voter = Integer.parseInt(input[0].trim());
 			int cand = Integer.parseInt(input[1].trim());
+			
+			//Checks if Voter has already voted, if so voter has committed fraud
 			if(voters.contains(voter)) {
 				fraud_voters.add(voter);
-				System.out.printf("Voter %07d commited Fraud!!!!%n", voter);
+				//System.out.printf("Voter %07d commited Fraud!!!!%n", voter);
+			// Otherwise add to count
 			} else {
 				voters.add(voter);
+				// Add to Associated Pool
 				if(cand_votes.containsKey(cand)) {
 					int votes = cand_votes.get(cand) + 1;
 					cand_votes.put(cand, votes);
+				// Create pool if doesn't exists
 				} else {
 					cand_votes.put(cand, 1);
 				}
 //				System.out.printf("Voter: %s, Candidate: %s %n", input[0], input[1]);	
 			}
 		}
+		// Produce Final Results
 		System.out.println("\nFinal Results");
 		System.out.println("~~~~~~~~~~~~~~~~~");
-		int best = 0;
+		
+		// Gets First Cand_Votes Key
+		int best = (int) cand_votes.keySet().toArray()[0];
+		
+		// Finds out which Candidate has the most votes and prints out winner
 		for(int i : cand_votes.keySet()) {
 			if(cand_votes.get(i) > cand_votes.get(best)) {
 				best = i;
@@ -43,6 +68,7 @@ public class Solver {
 		}
 		System.out.printf("%nCandidate %02d Won! %n", best);
 		
+		// Lists each Voter who Committed Fraud
 		System.out.printf("%nCount: %d%nFraud Voters%n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%n", fraud_voters.size());
 		for(int f : fraud_voters) {
 			System.out.printf("Voter #: %07d %n", f);
